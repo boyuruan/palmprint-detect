62bccb6258e14c8877ec647a3be146b7e3b25e76
boyuruan <boyuruan@gmail.com>
2014-07-29 14:09:52 +0800

    Update Mask.cpp

--- Mask.cpp
+++ Mask.cpp
@@ -7,53 +7,55 @@
 using namespace cv;
 using namespace std;
 
-void block_searching(vector<vector<double>>, Mat, Mat, int);
+vector<vector<double>> block_searching(Mat, Mat, int);
 
 void Mask(Mat* A, Mat* B, int numberOfFrames)
 {
 	Mat M = Mat::zeros(Size(A[0].cols, A[0].rows), CV_8U);
 	vector<vector<double>> Colors(3, vector<double>(0));
-	
+	vector<vector<double>> Colors_t(3, vector<double>(0));
 	int count = 0;
 	for (; count < numberOfFrames; count++)
 	{
-		block_searching(Colors, A[count], B[count], numberOfFrames);
+		Colors = block_searching(Colors, A[count], B[count]);
+		
+		for (int k = 0; k < Colors[0].size(); k++)
+		{
+			Colors_t[0].push_back(Colors[0][k]);
+			Colors_t[1].push_back(Colors[1][k]);
+			Colors_t[2].push_back(Colors[2][k]);
+		}
 	}
-	int total = Colors[0].size();
+	int total = Colors_t[0].size();
 	Mat Data = Mat(total, 3, CV_8U);
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < total; j++)
-			Data.at<double>(Point(i,j)) = Colors[i][j];
+			Data.at<double>(Point(i,j)) = Colors_t[i][j];
 	Mat to_progress = A[numberOfFrames - 1];
-	delete A;
-	delete B;
+	
 }
 
-void block_searching(vector<vector<double>> Colors, Mat original, Mat foreground, int numberOfFrames)
+vector<vector<double>> block_searching(Mat original, Mat foreground)
 {
-	int h = foreground.rows / 20;
-	int w = foreground.cols / 20;
+	vector<vector<double>> Colors(3, vector<double>(0));
+	int h = foreground.rows / 10;
+	int w = foreground.cols / 10;
 	int t1, t2;
 	for (t1 = 0; t1 < h; t1++)
 	{
 		for (t2 = 0; t2 < w; t2++)
 		{
-			Rect r(t2 * 20, t1 * 20, 20, 20);
+			Rect r(t2 * 10, t1 * 10, 10, 10);
 			Mat Sub = foreground(r);
-			if (countNonZero(Sub) <= 40)
+			if (countNonZero(Sub) <= 10)
 			{
 				Mat Sub_C = original(r);
-				for (int i = 0; i < 20; i++)
-				{
-					for (int j = 0; j < 20; j++)
-					{
-						Vec3f indensity = Sub_C.at<Vec3f>(Point(i, j));
-						Colors[0].push_back(indensity.val[0]);
-						Colors[1].push_back(indensity.val[1]);
-						Colors[2].push_back(indensity.val[2]);
-					}
-				}
+				Scalar M = mean(Sub_C);
+				Colors[0].push_back(M.val[0]);
+				Colors[1].push_back(M.val[1]);
+				Colors[2].push_back(M.val[2]);
 			}
 		}
 	}
+	return Colors;
 }
