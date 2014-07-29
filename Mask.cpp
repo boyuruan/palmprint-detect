fa06edb11a8ad34a9bf2fb4b013a487abc3bc8a1
boyuruan <boyuruan@gmail.com>
2014-07-29 15:46:18 +0800

    Update Mask.cpp

--- Mask.cpp
+++ Mask.cpp
@@ -11,7 +11,7 @@ vector<vector<double>> block_searching(Mat, Mat, int);
 
 void Mask(Mat* A, Mat* B, int numberOfFrames)
 {
-	Mat M = Mat::zeros(Size(A[0].cols, A[0].rows), CV_8U);
+	Mat M = Mat::zeros(Size(A[0].cols, A[0].rows), CV_64F);
 	vector<vector<double>> Colors(3, vector<double>(0));
 	vector<vector<double>> Colors_t(3, vector<double>(0));
 	int count = 0;
@@ -27,12 +27,36 @@ void Mask(Mat* A, Mat* B, int numberOfFrames)
 		}
 	}
 	int total = Colors_t[0].size();
-	Mat Data = Mat(total, 3, CV_8U);
+	Mat Data = Mat(total, 3, CV_64F);
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < total; j++)
 			Data.at<double>(Point(i,j)) = Colors_t[i][j];
 	Mat to_progress = A[numberOfFrames - 1];
-	
+	Mat cov;
+	Mat MM;
+	calcCovarMatrix(Data, cov, MM, CV_COVAR_ROWS|CV_COVAR_NORMAL);
+	cov = cov / (double)(total - 1);
+	int x, y;
+	for (x = 0; x < to_progress.cols; x++)
+	{
+		for (y = 0; y < to_progress.rows; y++)
+		{
+			Vec3b indensity = to_progress.at<Vec3b>(Point(x, y));
+			uchar to_b = indensity.val[0];
+			uchar to_g = indensity.val[1];
+			uchar to_r = indensity.val[2];
+			Mat E = Mat(3, 1, CV_64F);
+			E.at<double>(Point(0, 0)) = (double)to_b - MM.at<double>(Point(0, 0));
+			E.at<double>(Point(0, 1)) = (double)to_g - MM.at<double>(Point(1, 0));
+			E.at<double>(Point(0, 2)) = (double)to_r - MM.at<double>(Point(2, 0));
+			Mat temp(1, 1, CV_64F);
+			temp = E.t()*cov.inv()*E;
+			Scalar ind = temp.at<double>(0, 0);
+			//OutFile << exp(ind.val[0]) << endl;
+			M.at<double>(Point(x, y)) = exp(ind.val[0]);
+		}
+	}
+	return M;
 }
 
 vector<vector<double>> block_searching(Mat original, Mat foreground)
