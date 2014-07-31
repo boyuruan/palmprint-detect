4c5356da117827c82e846607c66fad6ee8949ffd
boyuruan <boyuruan@gmail.com>
2014-07-31 16:09:16 +0800

    Update Mask.cpp

--- Mask.cpp
+++ Mask.cpp
@@ -11,7 +11,7 @@ vector<vector<double>> block_searching(Mat, Mat, int);
 
 void Mask(Mat* A, Mat* B, int numberOfFrames)
 {
-	Mat M = Mat::zeros(Size(A[0].cols, A[0].rows), CV_64F);
+	Mat M = Mat::zeros(Size(A[0].cols, A[0].rows), CV_64FC1);
 	vector<vector<double>> Colors(3, vector<double>(0));
 	vector<vector<double>> Colors_t(3, vector<double>(0));
 	int count = 0;
@@ -32,6 +32,7 @@ void Mask(Mat* A, Mat* B, int numberOfFrames)
 		for (int j = 0; j < total; j++)
 			Data.at<double>(Point(i,j)) = Colors_t[i][j];
 	Mat to_progress = A[numberOfFrames - 1];
+	imwrite("to_progress.jpg", to_progress);
 	Mat cov;
 	Mat MM;
 	calcCovarMatrix(Data, cov, MM, CV_COVAR_ROWS|CV_COVAR_NORMAL);
@@ -45,14 +46,13 @@ void Mask(Mat* A, Mat* B, int numberOfFrames)
 			uchar to_b = indensity.val[0];
 			uchar to_g = indensity.val[1];
 			uchar to_r = indensity.val[2];
-			Mat E = Mat(3, 1, CV_64F);
+			Mat E = Mat(3, 1, CV_64FC1);
 			E.at<double>(Point(0, 0)) = (double)to_b - MM.at<double>(Point(0, 0));
 			E.at<double>(Point(0, 1)) = (double)to_g - MM.at<double>(Point(1, 0));
 			E.at<double>(Point(0, 2)) = (double)to_r - MM.at<double>(Point(2, 0));
 			Mat temp(1, 1, CV_64F);
 			temp = E.t()*cov.inv()*E;
 			Scalar ind = temp.at<double>(0, 0);
-			//OutFile << exp(ind.val[0]) << endl;
 			M.at<double>(Point(x, y)) = exp(ind.val[0]);
 		}
 	}
